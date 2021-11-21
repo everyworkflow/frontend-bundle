637c76d42f80c8e5ade4bddaebd4e9bcf00fc3de
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:03:29 +0000

    Added ?ExtensionInterface

--- EveryWorkflowFrontendBundle.php
+++ EveryWorkflowFrontendBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\FrontendBundle;
 
 use EveryWorkflow\FrontendBundle\DependencyInjection\FrontendExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowFrontendBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new FrontendExtension();
     }
